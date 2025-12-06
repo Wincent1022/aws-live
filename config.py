263eeccad004bb37f89b70f469c17833f034441c
Wincent1022 <wincentkxw-wp22@student.tarc.edu.my>
2025-12-06 11:10:32 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.czhjyapojrii.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "employee"
+customregion = "us-east-1"
+
